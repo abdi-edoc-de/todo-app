3a0bfa5d9be00db89af510a0ecdf57fc93c0ffbe
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2021-02-27 19:18:07 +0300

    [init] initial upload

--- app/src/main/java/com/example/schedule/TaskAdapter.kt
+++ app/src/main/java/com/example/schedule/TaskAdapter.kt
@@ -1,16 +1,20 @@
 package com.example.schedule
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schedule.models.Task
 import java.text.SimpleDateFormat
 
-class TaskAdapter(private val taskLists: List<Task>): RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
-//    private lateinit var card:LinearLayout;
+
+class TaskAdapter(private val taskLists: List<Task>, private val context: Context): RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+    private lateinit var card:LinearLayout;
 
     inner class ViewHolder(listItemView: View): RecyclerView.ViewHolder(listItemView){
          val subtaskConfirm : LinearLayout = itemView.findViewById(R.id.subCinfirmation);
@@ -33,21 +37,23 @@ class TaskAdapter(private val taskLists: List<Task>): RecyclerView.Adapter<TaskA
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val taskItem: Task = taskLists[position]
         // Set item views based on your views and data model
-//        card=holder.subtaskConfirm
+        card=holder.subtaskConfirm
         val textView = holder.titleView
         textView.text = taskItem.title
         val dateView = holder.dateView
         val timeFormat = SimpleDateFormat("hh:mm aa")
         dateView.text = "${timeFormat.format(taskItem.startDate)}-${timeFormat.format(taskItem.endDate)}"
-
-
+        card.setOnClickListener{
+            var popUp: DialogFragment = DialogFragment()
+            popUp.show((context as Fragment).parentFragmentManager, "dont")
+        }
     }
 
     override fun getItemCount(): Int {
         return taskLists.size
     }
-//    fun getCard():LinearLayout {
-//        return card
-//    }
+    fun getCard():LinearLayout {
+        return card
+    }
 
 }
