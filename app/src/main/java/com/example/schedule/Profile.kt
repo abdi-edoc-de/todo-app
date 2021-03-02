10fd4b6de0edbc8b95057a705ce4c04d07912451
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2021-03-02 18:34:05 +0300

    [init] initial upload

--- app/src/main/java/com/example/schedule/Profile.kt
+++ app/src/main/java/com/example/schedule/Profile.kt
@@ -7,18 +7,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.room.Room
 import com.example.schedule.databinding.FragmentProfileBinding
 import com.example.schedule.models.User
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.philjay.circledisplay.CircleDisplay
+import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -47,7 +45,6 @@ private lateinit var binding:FragmentProfileBinding;
 
 
 
-//        var chart: PieChart =binding.pi;
 //        var arr=ArrayList<PieEntry>()
 //        arr.add(PieEntry(200.0F, "asd"))
 //        arr.add(PieEntry(20.0F, "asd"))
@@ -69,31 +66,68 @@ private lateinit var binding:FragmentProfileBinding;
 
 //        the left chart
 
+
+        var chart: PieChart =binding.pi;
+        var arr1=ArrayList<PieEntry>()
+        arr1.add(PieEntry(200.0F))
+        arr1.add(PieEntry(110.0F))
+
+        var pieDataSet1: PieDataSet = PieDataSet(arr1,"")
+        pieDataSet1.valueTextSize=16f
+        pieDataSet1.colors= ColorTemplate.COLORFUL_COLORS.toList()
+
+
+        var pieData1= PieData(pieDataSet1)
+        chart.setBackgroundColor(Color.TRANSPARENT)
+        chart.data=pieData1
+        chart.centerText="20%"
+        chart.setCenterTextColor(Color.WHITE)
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.TRANSPARENT);
+        chart.description.isEnabled = false
+        chart.setDrawSlicesUnderHole(false)
+        chart.data.setDrawValues(false)
+
+        chart.legend.isEnabled=false;
+        chart.setDrawEntryLabels(false)
+        chart.setDrawMarkers(false)
+        chart.animate()
+
+
+
+
         var chart1: PieChart =binding.pi2;
         var arr2=ArrayList<PieEntry>()
         arr2.add(PieEntry(200.0F))
         arr2.add(PieEntry(110.0F))
 
         var pieDataSet2: PieDataSet = PieDataSet(arr2,"")
-        pieDataSet2.valueTextColor= Color.BLACK
-        pieDataSet2.colors= listOf(
-            Color.rgb(193, 37, 82), Color.rgb(255, 102, 0), Color.rgb(245, 199, 0),
-            Color.rgb(106, 150, 31), Color.rgb(179, 100, 53)
-        )
         pieDataSet2.valueTextSize=16f
+        pieDataSet2.colors= ColorTemplate.COLORFUL_COLORS.toList()
+
+
         var pieData2= PieData(pieDataSet2)
         chart1.setBackgroundColor(Color.TRANSPARENT)
         chart1.data=pieData2
         chart1.centerText="20%"
+        chart1.setCenterTextColor(Color.WHITE)
         chart1.setDrawHoleEnabled(true);
         chart1.setHoleColor(Color.TRANSPARENT);
         chart1.description.isEnabled = false
+        chart1.setDrawSlicesUnderHole(false)
+        chart1.data.setDrawValues(false)
+
 
         chart1.legend.isEnabled=false;
         chart1.setDrawEntryLabels(false)
         chart1.setDrawMarkers(false)
-        chart1.setDrawSliceText(false)
         chart1.animate()
+
+
+
+
+
+
 
         var user: User
         GlobalScope.launch {
@@ -119,21 +153,21 @@ private lateinit var binding:FragmentProfileBinding;
 
 
 //the green ring chart
-        val cd:CircleDisplay = binding.circleDisplay
-        cd.setAnimDuration(2000)
-        cd.setValueWidthPercent(55f)
-        cd.setTextSize(18f)
-        cd.setColor(Color.GREEN)
-        cd.setDrawText(true)
-        cd.setDrawInnerCircle(true)
-        cd.setFormatDigits(1)
-        cd.setTouchEnabled(false)
-//        cd.setSelectionListener(requireActivity())
-        cd.setUnit("%")
-        cd.setStepSize(0.5f)
-        // cd.setCustomText(...); // sets a custom array of text
-        // cd.setCustomText(...); // sets a custom array of text
-        cd.showValue(75f, 100f, true)
+//        val cd:CircleDisplay = binding.circleDisplay
+//        cd.setAnimDuration(2000)
+//        cd.setValueWidthPercent(55f)
+//        cd.setTextSize(18f)
+//        cd.setColor(Color.GREEN)
+//        cd.setDrawText(true)
+//        cd.setDrawInnerCircle(true)
+//        cd.setFormatDigits(1)
+//        cd.setTouchEnabled(false)
+////        cd.setSelectionListener(requireActivity())
+//        cd.setUnit("%")
+//        cd.setStepSize(0.5f)
+//        // cd.setCustomText(...); // sets a custom array of text
+//        // cd.setCustomText(...); // sets a custom array of text
+//        cd.showValue(75f, 100f, true)
 
 
 
